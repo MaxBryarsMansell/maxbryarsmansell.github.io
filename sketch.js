a40427bc370fa030669ac17a6fd5051aabb2690e
Max <maxbm46@gmail.com>
2018-02-16 20:35:39 +0000

    hmmmm

--- sketch.js
+++ sketch.js
@@ -1,58 +1,42 @@
-var gravity;
-var time = 0
-var lastTime = 0
 
 
 function setup() {
-    createCanvas(1000,500);
-    frameRate(60)
-    e = new Player(width/2,height/2);
+    createCanvas(600,500);
+    frameRate(60);
+    background(52);
+    maxIterations = 30;
+    xFactor = 3 / width;
+    yFactor = 3 / height;
+    done = false;
 }
 
 function draw() {
-    dt = calcDt() / 1000
-    gravity = (abs(e.v.x) + 5) / 25
-    background(51);
-    
-    e.update(gravity,dt * 100);
-    e.collides();
+  if (!done){
+    for (var y = 0; y < height; y++){
+      cIm = -1.5 + (yFactor * y);
+      for (var x = 0; x < width; x++){
+        cRe = -2 + (xFactor * x);
 
-    e.display();
+        zRe = cRe, zIm = cIm;
+        isInside = true;
+        for (var i = 0; i < maxIterations; i++){
+          zReSquare = zRe * zRe;
+          zImSquare = zIm * zIm;
+          if(zReSquare + zImSquare > 4){
+            isInside = false;
+            break;
+          }
+          zIm = (2*zRe*zIm) + cIm;
+          zRe = (zReSquare - zImSquare) + cRe;
 
-    fill(54,175,194);
-    textSize(32);
-    text("Gravity: " + round(gravity * 100),15,40)
-    
-}
-
-
-function keyPressed() {
-    if (keyCode === 68) {
-        e.tv.x = 20;
-    } 
-    if (keyCode === 65) {
-        e.tv.x = -20;
-    } 
-    if (keyCode === 32 && e.canJump()) {
-        e.jump();
-    } 
-}
-function keyReleased() {
-    if (keyCode === 68) {
-        e.tv.x = 0;
-    } 
-    if (keyCode === 65) {
-        e.tv.x = 0;
-    } 
-
-}
-
-calcDt = function(){
-    lastTime = time
-    time = millis()
-    dt = time - lastTime
-    if (dt > 150){
-        dt = 150
+        }
+        if (isInside){
+          c = color(zReSquare * 255, cRe * 255, zRe * 255)
+          stroke(c);
+          point(x, y);
+        }
+      }
     }
-    return dt
+    done = true;
+  }
 }
