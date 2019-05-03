211c5761646019e45bedda938b1d4e141c4f8147
Max Bryars-Mansell <maxbm46@gmail.com>
2019-05-03 19:16:49 +0100

    Small improvements

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
-const GRAVITATIONAL_CONSTANT = 0.001;
+const GRAVITATIONAL_CONSTANT = 0.002;
 const PI = 3.14159;
 const OBJECT_DENSITY = 0.5;
-const MAX_OBJECT_RADIUS = 100;
+const MAX_OBJECT_RADIUS = 80;
 const MIN_OBJECT_RADIUS = 20;
 const MAX_OBJECT_MASS = 1.333 * PI * MAX_OBJECT_RADIUS * MAX_OBJECT_RADIUS * MAX_OBJECT_RADIUS * OBJECT_DENSITY;
-const MAX_OBJECTS = 10;
+const MAX_OBJECTS = 15;
+const SPAWN_CHANCE = 0.1;
 
 function Object(radius, x, y, isFixed) {
 	this.position = createVector(x, y);
 	this.velocity = createVector(Math.random() * 10 - 5, Math.random() * 10 - 5);
 	this.acceleration = createVector(0, 0);
-	this.collided = false;
-	this.fixed = isFixed;
 	this.setRadius(radius);
+	this.fixed = isFixed;
 
+	this.collided = false;
 	this.history = [];
 	this.last = millis()
 	this.timer = 0;
@@ -42,14 +43,11 @@ Object.prototype = {
 	},
 
 	draw: function () {
-		fill(255);
+		fill(255 - this.mass / MAX_OBJECT_MASS * 150);
 		for (l = 0; l < this.history.length; l++){
 			pos = this.history[l];
 			ellipse(pos.x, pos.y, 5, 5);
 		}
-
-
-		fill(255 - this.mass / MAX_OBJECT_MASS * 150);
 		ellipse(this.position.x, this.position.y, this.radius, this.radius);
 	},
 
@@ -65,17 +63,15 @@ function setup() {
 	canvas = createCanvas(windowWidth, windowHeight);
 	canvas.position(0, 0)
 	canvas.style('z-index', -1)
-	objects.push(new Object(100, 0.25 * windowWidth, 0.25 * windowHeight, true))
-	objects.push(new Object(100, 0.25 * windowWidth, 0.75 * windowHeight, true))
-	objects.push(new Object(100, 0.75 * windowWidth, 0.25 * windowHeight, true))
-	objects.push(new Object(100, 0.75 * windowWidth, 0.75 * windowHeight, true))
+	objects.push(new Object(MAX_OBJECT_RADIUS, 0.75 * windowWidth, 0.5 * windowHeight, true))
+	objects.push(new Object(MAX_OBJECT_RADIUS, 0.25 * windowWidth, 0.5 * windowHeight, true))
 }
 
 function draw() {
 	background(40);
 
 	for (p = objects.length - 1; p >= 0; p--) { if (objects[p].collided) { objects.splice(p, 1) } }
-	if (Math.random() < 0.1 && objects.length < MAX_OBJECTS) { objects.push(new Object(Math.random() * (MAX_OBJECT_RADIUS - MIN_OBJECT_RADIUS) + MIN_OBJECT_RADIUS, Math.random() * windowWidth, Math.random() * windowHeight, false)) }
+	if (Math.random() < SPAWN_CHANCE && objects.length < MAX_OBJECTS) { objects.push(new Object(Math.random() * (MAX_OBJECT_RADIUS - MIN_OBJECT_RADIUS) + MIN_OBJECT_RADIUS, Math.random() * windowWidth, Math.random() * windowHeight, false)) }
 
 	for (i = 0; i < objects.length; i++) {
 		forceSum = createVector(0, 0);
@@ -83,7 +79,6 @@ function draw() {
 			if (i == j) { continue; }
 			dist = p5.Vector.dist(objects[i].position, objects[j].position);
 			if (dist < 0.5 * (objects[i].radius + objects[j].radius) && objects[i].radius > objects[j].radius) {
-				objects[i].setRadius(objects[i].radius + objects[j].radius / 100)
 				objects[j].collided = true;
 				break;
 			}
@@ -96,6 +91,6 @@ function draw() {
 }
 
 function mouseClicked() {
-	object = new Object(Math.random() * MAX_OBJECT_RADIUS, mouseX, mouseY, false)
+	object = new Object(Math.random() * (MAX_OBJECT_RADIUS - MIN_OBJECT_RADIUS) + MIN_OBJECT_RADIUS, mouseX, mouseY, false)
 	objects.push(object)
 }
